d4c583a8c1fe999b2b3a1c5867659d67494cc9e2
Byuntil <gugugu2014@jbnu.ac.kr>
2021-07-31 22:25:23 +0900

    chapter 5

--- src/Controllers/videoContoller.js
+++ src/Controllers/videoContoller.js
@@ -1,5 +1,32 @@
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", potato: "me" }); //변수도 보낼수 있음
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos: videos }); //변수도 보낼수 있음
+};
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search", { pageTitle: "Search" });
